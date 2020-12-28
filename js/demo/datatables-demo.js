3aa9cfc9f07c08c00ed048059c94d7f1735bdc8a
angelo.conejeros@nivelat.com <angelo.conejeros@nivelat.com>
2020-12-28 15:10:13 -0300

    second commit

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,4 +1,14 @@
-// Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable(
+    {
+      "language": {
+        "search": "Filtrar resultados:",
+        "lengthMenu": "Mostrando _MENU_  Registros por pagina",
+        "zeroRecords": "Sin resultados - sorry",
+        "info": "Mostrando pagina _PAGE_ de _PAGES_",
+        "infoEmpty": "No hay registros disponibles",
+        "infoFiltered": "(filtrados de _MAX_ registros totales)"
+      }
+    }
+  );
 });
